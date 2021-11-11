c43d1abb996a17b5b6a7d4a85b77a5a633a9ee16
Markus <70133910+MarkusG94@users.noreply.github.com>
2021-11-11 15:47:54 +0100

    still trying to make the routing work

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.use( '/matchWinners', matchWinners )
 
 // Behövs om man använder React Router
 app.get('*', (req, res) => {
-	res.sendFile(__dirname + '/../build/index.html')
+	res.sendFile(__dirname + '/build/index.html')
 })
 
 app.listen(PORT, () => {
