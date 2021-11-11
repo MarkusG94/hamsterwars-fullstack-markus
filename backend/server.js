70e16da72f51705a76aeef58b54418e7351e71ab
Markus <70133910+MarkusG94@users.noreply.github.com>
2021-11-11 17:27:51 +0100

    changed order on some code

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
-const path = require('path');
+const app = express()
+
 
 const PORT = process.env.PORT || 1337
 
