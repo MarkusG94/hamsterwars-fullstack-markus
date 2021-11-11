ff0b6afaf8f2c5fb23cca2854e0277101df19aa8
Markus <70133910+MarkusG94@users.noreply.github.com>
2021-11-11 22:51:50 +0100

    moved navbar to its own component again

--- src/App.tsx
+++ src/App.tsx
@@ -11,21 +11,9 @@ function App() {
 	return (
 		<div className="app">
 			
-				<nav className="nav">
-                <section className="nav-logo-text">
-                <img className="nav-logo" src="img/hamster-logo.svg" alt ="a hamster" />
-                <h1 className="nav-title">HAMSTERWARS</h1>
-                </section> 
-                <section className="nav-links">
-				<Link className="nav-link" to="/"><p className="nav-text">Home</p></Link>
-				<Link className="nav-link" to="/battle"><p className="nav-text">Battle</p></Link>
-				<Link className="nav-link" to="/gallery"><p className="nav-text">Gallery</p></Link>
-                {/* <button className="nav-button">something</button> */}
-                </section>
 
-			</nav>
 
-			{/* <Nav /> */}
+			 <Nav /> 
 		<main className="app-main">
 
 		<Switch>
